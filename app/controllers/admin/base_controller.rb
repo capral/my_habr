7da66d279f19fdd1ccb5d25e73998e43cee02f09
Maxim Nyrko <max.nyrko@mail.ru>
2015-11-30 21:22:08 +0300

    3-th commit

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,4 @@
-  class Admin::BaseController < Admin::ApplicationController
+  class Admin::BaseController < ApplicationController
 
     before_action :authenticate_user!
     before_action :cheсk_admin
